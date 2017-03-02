c25c0b7b8f33f9802baa563ca0ea6b91ea4a31e3
69pmb <pmbroca@gmail.com>
2017-03-02 17:28:03 +0100

    Nom du fichier cleaned, stripAccent dans main

--- pmb/music/AllMusic/file/CleanFile.java
+++ pmb/music/AllMusic/file/CleanFile.java
@@ -4,10 +4,14 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.text.Normalizer;
+import java.text.Normalizer.Form;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
@@ -27,7 +31,7 @@ public class CleanFile {
         BufferedReader br = null;
         String line = "";
         BufferedWriter writer = null;
-        String exitFile = file.getParentFile().getAbsolutePath() + "\\" + "Cleaned - " + file.getName();
+        String exitFile = file.getParentFile().getAbsolutePath() + "\\" + StringUtils.substringBeforeLast(file.getName(),".") + " - Cleaned." + StringUtils.substringAfterLast(file.getName(),".");
 
         try {
             br = new BufferedReader(new InputStreamReader(new FileInputStream(file), Constant.ANSI_ENCODING));
@@ -80,4 +84,48 @@ public class CleanFile {
         }
         return isDigit;
     }
+    
+    public static void main(String[] args) {
+        System.out.println("Start clearFile");
+        File file = new File(Constant.FINAL_FILE_PATH);
+        List<String> sepAsList = new LinkedList<String>(Arrays.asList(Constant.SEPARATORS));
+        BufferedReader br = null;
+        String line = "";
+        BufferedWriter writer = null;
+        String exitFile = file.getParentFile().getAbsolutePath() + "\\" + StringUtils.substringBeforeLast(file.getName(),".") + " - Cleaned." + StringUtils.substringAfterLast(file.getName(),".");
+
+        try {
+            try {
+                br = new BufferedReader(new InputStreamReader(new FileInputStream(file), Constant.UTF8_ENCODING));
+                writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(exitFile), Constant.UTF8_ENCODING));
+            } catch (UnsupportedEncodingException | FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            try {
+                while ((line = br.readLine()) != null) {
+                    writer.append(Normalizer.normalize(line, Form.NFKD)
+                            .replaceAll("\\p{InCombiningDiacriticalMarks}+", "")).append("\n");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (writer != null) {
+                try {
+                    writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        System.out.println("End clearFile");
+    }
+
 }
