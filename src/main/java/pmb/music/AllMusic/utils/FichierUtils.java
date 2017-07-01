e7d9d43e1aee8e08d6bf288ede796153d1f346ab
Pierre-Marie <Pierre-Marie@192.168.1.21>
2017-07-01 18:34:00 +0200

    Clean history

--- src/main/java/pmb/music/AllMusic/utils/FichierUtils.java
+++ src/main/java/pmb/music/AllMusic/utils/FichierUtils.java
@@ -3,12 +3,20 @@
  */
 package pmb.music.AllMusic.utils;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.Vector;
 
+import org.apache.commons.lang3.StringUtils;
 import org.codehaus.plexus.util.FileUtils;
 
 import pmb.music.AllMusic.model.Composition;
@@ -65,5 +73,40 @@ public class FichierUtils {
 		if (!FileUtils.fileExists(nomDir)) {
 			FileUtils.mkdir(nomDir);
 		}
+	}
+
+	public static void cleanHistory() throws ParseException, IOException {
+		System.out.println("Start cleanHistory");
+		// Création d'une map avec:
+		// key nom du fichier sans date
+		// value liste des dates du fichier
+		List<File> files = new ArrayList<File>();
+		CompositionUtils.listFilesForFolder(new File(Constant.HISTORY_PATH), files, Constant.XML_EXTENSION, false);
+		Map<String, List<Date>> list = new HashMap<String, List<Date>>();
+		for (File file : files) {
+			String nomFichier = StringUtils.substringBefore(file.getName(), Constant.SEPARATOR_DATE_HISTORY);
+			String date = StringUtils.substringBetween(file.getName(), Constant.SEPARATOR_DATE_HISTORY,
+					Constant.XML_EXTENSION);
+			if (list.get(nomFichier) == null) {
+				list.put(nomFichier, new ArrayList<Date>());
+			}
+			list.get(nomFichier).add(Constant.SDF_HISTORY.parse(date));
+		}
+
+		Set<String> keySet = list.keySet();
+		for (String key : keySet) {
+			// Tri des dates, la plus récente en 1er
+			Collections.sort(list.get(key), Collections.reverseOrder());
+			String path = Constant.HISTORY_PATH + "//" + key + Constant.SEPARATOR_DATE_HISTORY;
+			// Suppression des fichiers sauf du 1er
+			for (int i = 1; i < list.get(key).size(); i++) {
+				String toDelete = path + Constant.SDF_HISTORY.format(list.get(key).get(i)) + Constant.XML_EXTENSION;
+				if (!new File(toDelete).delete()) {
+					System.out.println(toDelete + " n'a pas pu etre supprimé");
+				}
+			}
+		}
+
+		System.out.println("End cleanHistory");
 	}
 }
