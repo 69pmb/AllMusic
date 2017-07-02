386a819b8a10583fbcd4e8e6c6ecc7eed61de1c2
Pierre-Marie <Pierre-Marie@192.168.1.21>
2017-07-02 11:14:22 +0200

    Ajout d'infobulles sur les boutons

--- src/main/java/pmb/music/AllMusic/file/CleanFile.java
+++ src/main/java/pmb/music/AllMusic/file/CleanFile.java
@@ -20,19 +20,24 @@ import pmb.music.AllMusic.utils.Constant;
 
 public class CleanFile {
 
-	private CleanFile(){};
-	
-    public static File clearFile(File file, boolean isSorted, String sep, String characterToRemove) throws IOException {
-        System.out.println("Start clearFile");
-        List<String> sepAsList = new LinkedList<>(Arrays.asList(Constant.SEPARATORS));
-        if (StringUtils.isNotBlank(sep)) {
-            sepAsList.add(sep);
-        }
-        String line = "";
-        String exitFile = file.getParentFile().getAbsolutePath() + "\\" + StringUtils.substringBeforeLast(file.getName(),".") + " - Cleaned." + StringUtils.substringAfterLast(file.getName(),".");
+	private CleanFile() {
+	};
 
-		try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file), Constant.ANSI_ENCODING));
-				BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(exitFile), Constant.ANSI_ENCODING));) {
+	public static File clearFile(File file, boolean isSorted, String sep, String characterToRemove) throws IOException {
+		System.out.println("Start clearFile");
+		List<String> sepAsList = new LinkedList<>(Arrays.asList(Constant.SEPARATORS));
+		if (StringUtils.isNotBlank(sep)) {
+			sepAsList.add(sep);
+		}
+		String line = "";
+		String exitFile = file.getParentFile().getAbsolutePath() + "\\"
+				+ StringUtils.substringBeforeLast(file.getName(), ".") + " - Cleaned."
+				+ StringUtils.substringAfterLast(file.getName(), ".");
+
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file),
+				Constant.ANSI_ENCODING));
+				BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(exitFile),
+						Constant.ANSI_ENCODING));) {
 			while ((line = br.readLine()) != null) {
 				boolean isDigit = true;
 				if (isSorted) {
@@ -52,34 +57,40 @@ public class CleanFile {
 			}
 			writer.flush();
 		}
-        System.out.println("End clearFile");
-        return new File(exitFile);
-    }
+		System.out.println("End clearFile");
+		return new File(exitFile);
+	}
 
-    private static boolean isBeginByDigit(String line) {
-        boolean isDigit = true;
-        String digitToTest = StringUtils.substring(line, 0, 1);
-        try {
-            Integer.parseInt(digitToTest);
-        } catch (NumberFormatException e) {
-            isDigit = false;
-        }
-        return isDigit;
-    }
-    
+	private static boolean isBeginByDigit(String line) {
+		boolean isDigit = true;
+		String digitToTest = StringUtils.substring(line, 0, 1);
+		try {
+			Integer.parseInt(digitToTest);
+		} catch (NumberFormatException e) {
+			isDigit = false;
+		}
+		return isDigit;
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Start clearFile");
 		File file = new File(Constant.FINAL_FILE_PATH);
-		List<String> sepAsList = new LinkedList<>(Arrays.asList(Constant.SEPARATORS));
+		// List<String> sepAsList = new
+		// LinkedList<>(Arrays.asList(Constant.SEPARATORS));
 		String line = "";
-		String exitFile = file.getParentFile().getAbsolutePath() + "\\" + StringUtils.substringBeforeLast(file.getName(), ".") + " - Cleaned."
+		String exitFile = file.getParentFile().getAbsolutePath() + "\\"
+				+ StringUtils.substringBeforeLast(file.getName(), ".") + " - Cleaned."
 				+ StringUtils.substringAfterLast(file.getName(), ".");
 
-		try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file), Constant.UTF8_ENCODING));
-			 BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(exitFile), Constant.UTF8_ENCODING));) {
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file),
+				Constant.UTF8_ENCODING));
+				BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(exitFile),
+						Constant.UTF8_ENCODING));) {
 			if (writer != null) {
 				while ((line = br.readLine()) != null) {
-					writer.append(Normalizer.normalize(line, Form.NFKD).replaceAll("\\p{InCombiningDiacriticalMarks}+", "")).append("\n");
+					writer.append(
+							Normalizer.normalize(line, Form.NFKD).replaceAll("\\p{InCombiningDiacriticalMarks}+", ""))
+							.append("\n");
 				}
 			}
 		} catch (IOException e) {
