d12f77d47d6e322e2631aab357408732d6d8fe5a
69pmb <pmbroca@gmail.com>
2017-07-04 17:27:51 +0200

    Update AppTest.java

--- src/main/java/pmb/music/AllMusic/AppTest.java
+++ src/main/java/pmb/music/AllMusic/AppTest.java
@@ -2,10 +2,7 @@ package pmb.music.AllMusic;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
@@ -13,12 +10,10 @@ import org.junit.Test;
 
 import pmb.music.AllMusic.file.ImportFile;
 import pmb.music.AllMusic.model.Cat;
-import pmb.music.AllMusic.model.Composition;
 import pmb.music.AllMusic.model.Fichier;
 import pmb.music.AllMusic.model.RecordType;
 import pmb.music.AllMusic.utils.CompositionUtils;
 import pmb.music.AllMusic.utils.Constant;
-import pmb.music.AllMusic.utils.SearchUtils;
 
 /**
  * Unit test for simple App.
@@ -51,38 +46,6 @@ public class AppTest {
 			LOG.debug("Separateur: " + ImportFile.getSeparator(randomLine.get(0)));
 			LOG.debug("############################################################################################################################################################################################################################################################\n");
 		}
-	}
-
-	// @Test
-	public void testSearch() {
-		List<Fichier> files = new ArrayList<Fichier>();
-		Fichier file1 = new Fichier("NME", "Best albums of the 60's", 2000, Cat.DECADE, 1960, 1969, false, 0,
-				new Date(), 50);
-		Fichier file2 = new Fichier("Pitchfork", "The best albums of The Beatles", 2010, Cat.MISCELLANEOUS, 1960, 1970,
-				true, 2, new Date(), 10);
-		files.add(file1);
-		files.add(file2);
-		Composition compo = new Composition("The Beatles", files, "Revolver", RecordType.ALBUM);
-
-		List<Fichier> files2 = new ArrayList<Fichier>();
-		Fichier file3 = new Fichier("NME", "Best albums of the 90's", 2000, Cat.DECADE, 1990, 1999, true, 3,
-				new Date(), 100);
-		Fichier file4 = new Fichier("GQ", "The best of the Rock", 2010, Cat.GENRE, 0, 0, true, 10, new Date(), 300);
-		files2.add(file3);
-		files2.add(file4);
-		Composition compo2 = new Composition("Oasis", files2, "Definitively Maybe", RecordType.ALBUM);
-
-		Composition compo3 = new Composition("Oasis", null, "What's The Story Morning Glory", RecordType.ALBUM);
-
-		List<Composition> compList = new ArrayList<Composition>();
-		compList.add(compo);
-		compList.add(compo2);
-		compList.add(compo3);
-
-		Map<String, String> criteria = new HashMap<>();
-		criteria.put("auteur", "nme");
-		List<Composition> search = SearchUtils.searchStrictly(compList, criteria);
-		CompositionUtils.printCompoList(search);
 	}
 
 	@Test
