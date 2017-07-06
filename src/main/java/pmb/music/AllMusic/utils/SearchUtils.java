638dfcb43df3c2a92cc5742c82cd827c68648c6e
69pmb <pmbroca@gmail.com>
2017-07-06 17:25:49 +0200

    Update SearchUtils.java

--- src/main/java/pmb/music/AllMusic/utils/SearchUtils.java
+++ src/main/java/pmb/music/AllMusic/utils/SearchUtils.java
@@ -19,8 +19,7 @@ import pmb.music.AllMusic.model.RecordType;
 /**
  * Contient les méthodes de recherche dans une liste de {@link Composition} avec
  * des critères de recherche.
- * 
- * @author i2113mj
+ * @author pmbroca
  */
 public class SearchUtils {
 
