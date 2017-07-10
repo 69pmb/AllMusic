6569755a223175ef90f7437e5bcf644ac415037b
69pmb <pmbroca@gmail.com>
2017-07-10 09:39:45 +0200

    sonar

--- src/main/java/pmb/music/AllMusic/view/DialogFileTable.java
+++ src/main/java/pmb/music/AllMusic/view/DialogFileTable.java
@@ -42,13 +42,13 @@ public class DialogFileTable extends JDialog {
 
 	private List<Fichier> files = new ArrayList<>();
 
-	private static final String[] title = { "Auteur", "Nom du fichier", "Date de publication", "Categorie", "Dates",
+	private static final String[] header = { "Auteur", "Nom du fichier", "Date de publication", "Categorie", "Dates",
 			"Date de création", "Taille", "Classement", "Classé" };
 
 	private JTable fichiers;
 
-	public DialogFileTable(JFrame parent, String title, boolean modal, List<Fichier> files, Dimension dim) {
-		super(parent, title, modal);
+	public DialogFileTable(JFrame parent, String header, boolean modal, List<Fichier> files, Dimension dim) {
+		super(parent, header, modal);
 		this.setSize(dim);
 		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
@@ -70,7 +70,7 @@ public class DialogFileTable extends JDialog {
 		fichiers.setBackground(UIManager.getColor("Label.background"));
 		fichiers.setFont(UIManager.getFont("Label.font"));
 		fichiers.setBorder(UIManager.getBorder("Label.border"));
-		fichiers.setModel(new FichierModel(FichierUtils.convertListForJTable(files), new Vector(Arrays.asList(title))));
+		fichiers.setModel(new FichierModel(FichierUtils.convertListForJTable(files), new Vector(Arrays.asList(header))));
 		fichiers.getRowSorter().toggleSortOrder(0);
 
 		TableColumnModel modelecolonne = fichiers.getColumnModel();
@@ -103,34 +103,38 @@ public class DialogFileTable extends JDialog {
 		return new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				if (e.getClickCount() == 2 && (e.getModifiers() & InputEvent.BUTTON1_MASK) != 0) {
-					LOG.debug("Start fichier mouse");
-					JTable target = (JTable) e.getSource();
-					Vector<String> v = (Vector<String>) ((FichierModel) target.getModel()).getDataVector().get(
-							target.getRowSorter().convertRowIndexToModel(target.getSelectedRow()));
-					String absFile = Constant.RESOURCES_ABS_DIRECTORY + v.get(1) + Constant.XML_EXTENSION;
-					try {
-						Runtime.getRuntime().exec(Constant.NOTEPAD_PATH + absFile);
-					} catch (IOException e1) {
-						LOG.error("", e1);
-					}
-					LOG.debug("End fichier mouse");
-				} else if (SwingUtilities.isRightMouseButton(e)) {
-					LOG.debug("Start right mouse");
-					JTable target = (JTable) e.getSource();
-					int rowAtPoint = target.rowAtPoint(SwingUtilities.convertPoint(target,
-							new Point(e.getX(), e.getY()), target));
-					if (rowAtPoint > -1) {
-						target.setRowSelectionInterval(rowAtPoint, rowAtPoint);
-					}
-					Vector<String> v = (Vector<String>) ((FichierModel) target.getModel()).getDataVector().get(
-							target.getRowSorter().convertRowIndexToModel(rowAtPoint));
-					StringSelection selection = new StringSelection(v.get(1));
-					Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-					clipboard.setContents(selection, selection);
-					LOG.debug("End right mouse");
-				}
+				mouseAction(e);
 			}
 		};
+	}
+
+	private void mouseAction(MouseEvent e) {
+		if (e.getClickCount() == 2 && (e.getModifiers() & InputEvent.BUTTON1_MASK) != 0) {
+			LOG.debug("Start fichier mouse");
+			JTable target = (JTable) e.getSource();
+			Vector<String> v = (Vector<String>) ((FichierModel) target.getModel()).getDataVector().get(
+					target.getRowSorter().convertRowIndexToModel(target.getSelectedRow()));
+			String absFile = Constant.RESOURCES_ABS_DIRECTORY + v.get(1) + Constant.XML_EXTENSION;
+			try {
+				Runtime.getRuntime().exec(Constant.NOTEPAD_PATH + absFile);
+			} catch (IOException e1) {
+				LOG.error("", e1);
+			}
+			LOG.debug("End fichier mouse");
+		} else if (SwingUtilities.isRightMouseButton(e)) {
+			LOG.debug("Start right mouse");
+			JTable target = (JTable) e.getSource();
+			int rowAtPoint = target.rowAtPoint(SwingUtilities.convertPoint(target,
+					new Point(e.getX(), e.getY()), target));
+			if (rowAtPoint > -1) {
+				target.setRowSelectionInterval(rowAtPoint, rowAtPoint);
+			}
+			Vector<String> v = (Vector<String>) ((FichierModel) target.getModel()).getDataVector().get(
+					target.getRowSorter().convertRowIndexToModel(rowAtPoint));
+			StringSelection selection = new StringSelection(v.get(1));
+			Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+			clipboard.setContents(selection, selection);
+			LOG.debug("End right mouse");
+		}
 	}
 }
