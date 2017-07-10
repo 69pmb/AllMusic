56c3f9ab7e8c7aedc0c08deb9787c2b3b76affc6
69pmb <pmbroca@gmail.com>
2017-07-10 09:40:03 +0200

    sonar

--- src/main/java/pmb/music/AllMusic/view/EvenOddRenderer.java
+++ src/main/java/pmb/music/AllMusic/view/EvenOddRenderer.java
@@ -11,6 +11,11 @@ import javax.swing.border.MatteBorder;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.TableCellRenderer;
 
+/**
+ * Classe decrivant le style des tableaux de l'application.
+ * Les couleurs des lignes sur 2.
+ * @author pmbroca
+ */
 public class EvenOddRenderer extends DefaultTableCellRenderer implements TableCellRenderer {
 
     private static final long serialVersionUID = 7366600520755781512L;
@@ -20,13 +25,12 @@ public class EvenOddRenderer extends DefaultTableCellRenderer implements TableCe
     @Override
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
         Component renderer = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
-        Color foreground, background;
+        Color foreground;
+        Color background;
         Color blue = new Color(47, 129, 210);
         Color darkBlue = new Color(33, 93, 153);
         Color gray = new Color(238, 229, 222);
         table.setBorder(noFocusBorder);
-        // if (hasFocus) {
-        // }
         if (isSelected) {
             setBorder(new MatteBorder(1, 0, 1, 0, Color.black));
             foreground = Color.black;
