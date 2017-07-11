7d3358e9dd85b4d67e46519b44f692e8a93fbad9
69pmb <pmbroca@gmail.com>
2017-07-11 10:20:51 +0200

    commentaire

--- src/main/java/pmb/music/AllMusic/view/BasicFrame.java
+++ src/main/java/pmb/music/AllMusic/view/BasicFrame.java
@@ -6,11 +6,19 @@ import javax.swing.JFrame;
 
 import pmb.music.AllMusic.utils.Constant;
 
+/**
+ * La fenetre principale contenant tous les composants de l'application.
+ * @see {@link JFrame}
+ * @author pmbroca
+ */
 public class BasicFrame extends JFrame{
     private static final long serialVersionUID = 3539279683629505967L;
     private Onglet tab;
     private MenuPanel menuPanel;
     
+    /**
+     * Construit la fenetre principale, ajoute le menu et les onglets.
+     */
     public BasicFrame(){
         super(Constant.DEFAULT_TITLE);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -19,7 +27,7 @@ public class BasicFrame extends JFrame{
         associateMenuPanel();
     }
     
-    public void associateMenuPanel(){
+    private void associateMenuPanel(){
         this.setMenuPanel(new MenuPanel(this));
         this.getContentPane().add(getMenuPanel(),BorderLayout.WEST);
         pack();
