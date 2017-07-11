1318010f42e4b3d85dc53ddb71523b178b869374
69pmb <pmbroca@gmail.com>
2017-07-11 10:54:57 +0200

    logs

--- src/main/java/pmb/music/AllMusic/view/MenuPanel.java
+++ src/main/java/pmb/music/AllMusic/view/MenuPanel.java
@@ -53,6 +53,7 @@ public class MenuPanel extends JPanel {
 	 * @return le {@link JMenuBar} crée
 	 */
 	public JMenuBar menuBar() {
+		LOG.debug("Start menuBar");
 		final JMenuBar menuBar = new JMenuBar();
 		
 		// Fichier
@@ -94,6 +95,7 @@ public class MenuPanel extends JPanel {
 				final int option = JOptionPane.showConfirmDialog(null, "Voulez-vous VRAIMENT quitter ?", "Demande confirmation ",
 						JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
 				if (option == 0) {
+					LOG.debug("Exit");
 					System.exit(0);
 				} else {
 					// Nothing to do
@@ -150,6 +152,7 @@ public class MenuPanel extends JPanel {
 		menuBar.add(aff);
 		menuBar.add(aide);
 
+		LOG.debug("End menuBar");
 		return menuBar;
 	}
 
