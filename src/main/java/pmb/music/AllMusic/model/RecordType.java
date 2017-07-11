856d4a2b79c12ace403e1c8323d860e66308f57a
69pmb <pmbroca@gmail.com>
2017-07-11 10:18:33 +0200

    commentaire

--- src/main/java/pmb/music/AllMusic/model/RecordType.java
+++ src/main/java/pmb/music/AllMusic/model/RecordType.java
@@ -3,6 +3,10 @@
  */
 package pmb.music.AllMusic.model;
 
+/**
+ * Enumeration pour le type d'une composition: chanson ou album.
+ * @author pmbroca
+ */
 public enum RecordType {
     SONG("SONG"),
     ALBUM("ALBUM"),
