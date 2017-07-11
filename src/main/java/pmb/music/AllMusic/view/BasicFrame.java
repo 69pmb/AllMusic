8871e7656954cdba76971bec100584a20c3abf12
69pmb <pmbroca@gmail.com>
2017-07-11 10:53:44 +0200

    logs

--- src/main/java/pmb/music/AllMusic/view/BasicFrame.java
+++ src/main/java/pmb/music/AllMusic/view/BasicFrame.java
@@ -4,6 +4,8 @@ import java.awt.BorderLayout;
 
 import javax.swing.JFrame;
 
+import org.apache.log4j.Logger;
+
 import pmb.music.AllMusic.utils.Constant;
 
 /**
@@ -11,6 +13,7 @@ import pmb.music.AllMusic.utils.Constant;
  * @see {@link JFrame}
  */
 public class BasicFrame extends JFrame{
+	private static final Logger LOG = Logger.getLogger(BasicFrame.class);
     private static final long serialVersionUID = 3539279683629505967L;
     private Onglet tab;
     private MenuPanel menuPanel;
@@ -20,17 +23,21 @@ public class BasicFrame extends JFrame{
      */
     public BasicFrame(){
         super(Constant.DEFAULT_TITLE);
+        LOG.debug("Start BasicFrame");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTab(new Onglet(this));
         this.getContentPane().add(tab,BorderLayout.EAST);
         associateMenuPanel();
+        LOG.debug("End BasicFrame");
     }
     
     private void associateMenuPanel(){
+        LOG.debug("Start associateMenuPanel");
         this.setMenuPanel(new MenuPanel(this));
         this.getContentPane().add(getMenuPanel(),BorderLayout.WEST);
         pack();
         setVisible(true);
+        LOG.debug("End associateMenuPanel");
     }
     
     public void setTab(final Onglet tab) {
