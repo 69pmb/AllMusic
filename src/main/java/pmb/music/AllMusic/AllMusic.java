c69a8514a6a02db3a4a271b78d6c5d97cecaffba
69pmb <pmbroca@gmail.com>
2017-07-26 14:04:25 +0200

    invokeLater on myFrame

--- src/main/java/pmb/music/AllMusic/AllMusic.java
+++ src/main/java/pmb/music/AllMusic/AllMusic.java
@@ -1,5 +1,7 @@
 package pmb.music.AllMusic;
 
+import java.awt.EventQueue;
+
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.UIManager;
@@ -16,41 +18,45 @@ import pmb.music.AllMusic.view.BasicFrame;
 public class AllMusic {
 
 	private static final Logger LOG = Logger.getLogger(AllMusic.class);
-	
-	private AllMusic(){}
-	
+
+	private AllMusic() {
+	}
+
 	/**
 	 * La méthode d'entrée du programme.
-	 * @param args 
+	 * 
+	 * @param args
 	 */
 	public static void main(String[] args) {
 		LOG.debug("Lancement de AllMusic");
-
 		Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
-
 			@Override
 			public void run() {
 				LOG.debug("Fin de AllMusic");
-				// Si je veux ajouter un traitement qui se lance quand l'appli se ferme
+				// Si je veux ajouter un traitement qui se lance quand l'appli
+				// se ferme
 			}
 		}));
 		try {
 			UIManager.setLookAndFeel("com.jtattoo.plaf.aluminium.AluminiumLookAndFeel");
-		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
-				| UnsupportedLookAndFeelException e) {
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
 			LOG.error("Impossible d'appliquer le style demandé", e);
 		}
-		
-		final BasicFrame f = new BasicFrame();
-		JFrame.setDefaultLookAndFeelDecorated(true);
-		JDialog.setDefaultLookAndFeelDecorated(true);
-		f.setExtendedState(JFrame.MAXIMIZED_BOTH);
-		try {
-			f.setLocation(null);
-		} catch (NullPointerException e) {
-			LOG.debug("",e);
-		}
-		f.pack();
-		f.setVisible(true);
+		EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				final BasicFrame f = new BasicFrame();
+				JFrame.setDefaultLookAndFeelDecorated(true);
+				JDialog.setDefaultLookAndFeelDecorated(true);
+				f.setExtendedState(JFrame.MAXIMIZED_BOTH);
+				try {
+					f.setLocation(null);
+				} catch (NullPointerException e) {
+					LOG.debug("", e);
+				}
+				f.pack();
+				f.setVisible(true);
+			}
+		});
 	}
 }
