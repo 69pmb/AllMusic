06163e867c8caa1dbf05fbca91bb085993320be1
69pmb <pmbroca@gmail.com>
2017-08-16 15:53:48 +0200

    ajout csv artistPanel

--- src/main/java/pmb/music/AllMusic/file/CsvFile.java
+++ src/main/java/pmb/music/AllMusic/file/CsvFile.java
@@ -31,12 +31,43 @@ public class CsvFile {
 		String name = filename + Constant.CSV_EXTENSION;
 		try (CSVWriter csvWriter = new CSVWriter(new OutputStreamWriter(new FileOutputStream(name), Constant.ANSI_ENCODING),
 				';');) {
+			String[] header = {"Artiste","Titre", "Type", "Nombre de fichiers"};
+			csvWriter.writeNext(header);
 			for (int i = 0; i < dataVector.size(); i++) {
 				Vector<Object> vector = dataVector.get(i);
 				String[] row = new String[4];
 				row[0] = (String) vector.get(0);
 				row[1] = (String) vector.get(1);
 				row[2] = (String) vector.get(2);
+				row[3] = String.valueOf((Integer) vector.get(3));
+				csvWriter.writeNext(row);
+			}
+		} catch (IOException e) {
+			LOG.error("Erreur lors de la génération du csv", e);
+		}
+		LOG.debug("End writeCsv");
+		return name;
+	}
+	
+	/**
+	 * Crée un fichier {@code CSV} à partir des résultats de recherche de l'écran artiste.
+	 * @param dataVector les données issues d'une recherche
+	 * @param filename le nom du fichier csv
+	 * @return  le nom du fichier
+	 */
+	public static String writeCsvFromArtistPanel(Vector<Vector<Object>> dataVector, String filename, String criteres) {
+		LOG.debug("Start writeCsv");
+		String name = filename + Constant.CSV_EXTENSION;
+		try (CSVWriter csvWriter = new CSVWriter(new OutputStreamWriter(new FileOutputStream(name), Constant.ANSI_ENCODING),
+				';');) {
+			String[] header = {"Artiste","Nombre d'occurences totales", "Albums", "Chansons", "Critères: " + criteres};
+			csvWriter.writeNext(header);
+			for (int i = 0; i < dataVector.size(); i++) {
+				Vector<Object> vector = dataVector.get(i);
+				String[] row = new String[4];
+				row[0] = (String) vector.get(0);
+				row[1] = String.valueOf((Integer) vector.get(1));
+				row[2] = String.valueOf((Integer) vector.get(2));
 				row[3] = String.valueOf((Integer) vector.get(3));
 				csvWriter.writeNext(row);
 			}
