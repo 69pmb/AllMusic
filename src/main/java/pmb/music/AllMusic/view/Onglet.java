f384ca5fcd387db36b17e3a5ea2a0d270b0de46d
69pmb <pmbroca@gmail.com>
2017-08-21 13:46:51 +0200

    Update Onglet.java

--- src/main/java/pmb/music/AllMusic/view/Onglet.java
+++ src/main/java/pmb/music/AllMusic/view/Onglet.java
@@ -65,17 +65,17 @@ public class Onglet extends JPanel {
     	LOG.debug("End Onglet");
     }
 
-	private List<String> getArtistList() {
+	public static List<String> getArtistList() {
 		return ImportXML.importXML(Constant.FINAL_FILE_PATH).stream().map(Composition::getArtist).map(WordUtils::capitalize)
 				.distinct().sorted().collect(Collectors.toList());
 	}
 
-	private List<String> getTitleList() {
+	public static List<String> getTitleList() {
 		return ImportXML.importXML(Constant.FINAL_FILE_PATH).stream().map(Composition::getTitre).map(WordUtils::capitalize)
 				.distinct().sorted().collect(Collectors.toList());
 	}
 
-	private List<String> getAuthorList() {
+	public static List<String> getAuthorList() {
 		return ImportXML.importXML(Constant.FINAL_FILE_PATH).stream().map(Composition::getFiles).flatMap(List::stream).map(Fichier::getAuthor)
 				.map(WordUtils::capitalize).distinct().sorted().collect(Collectors.toList());
 	}
