0f32c3c2994094fdf4dfcafd784db89df58d68a6
69pmb <pmbroca@gmail.com>
2017-08-21 13:47:07 +0200

    Update SearchPanel.java

--- src/main/java/pmb/music/AllMusic/view/SearchPanel.java
+++ src/main/java/pmb/music/AllMusic/view/SearchPanel.java
@@ -542,6 +542,7 @@ public class SearchPanel extends JPanel {
 		importXML.remove(indexOfXml);
 		compoResult.remove(indexOfResult);
 		Composition compoExist = CompositionUtils.compoExist(importXML, toModif);
+		// TODO ne pas utiliser compoExists
 		if (compoExist == null) {
 			importXML.add(toModif);
 			compoResult.add(toModif);
