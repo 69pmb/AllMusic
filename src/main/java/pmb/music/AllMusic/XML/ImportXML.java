2809481787c794f6f1965d83a022823e83f4c048
69pmb <pmbroca@gmail.com>
2017-08-25 11:48:24 +0200

    fusion en 2 listes + modif %

--- src/main/java/pmb/music/AllMusic/XML/ImportXML.java
+++ src/main/java/pmb/music/AllMusic/XML/ImportXML.java
@@ -7,13 +7,12 @@ import java.awt.Color;
 import java.awt.Font;
 import java.io.File;
 import java.io.IOException;
-import java.text.DecimalFormat;
-import java.text.DecimalFormatSymbols;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Locale;
 
 import javax.swing.JTextArea;
 import javax.xml.parsers.ParserConfigurationException;
@@ -24,26 +23,29 @@ import org.apache.log4j.Logger;
 import org.xml.sax.SAXException;
 
 import pmb.music.AllMusic.model.Composition;
+import pmb.music.AllMusic.model.RecordType;
 import pmb.music.AllMusic.utils.CompositionUtils;
 import pmb.music.AllMusic.utils.Constant;
 
 /**
  * Classe pour manipuler les fichiers XML.
+ * 
  * @see #importXML(String)
  * @see #fusionFiles(String, boolean)
  */
 public final class ImportXML {
 
 	private static final Logger LOG = Logger.getLogger(ImportXML.class);
-	private static final DecimalFormat DECIMAL_FORMAT = new DecimalFormat("#0.00", new DecimalFormatSymbols(Locale.US));
 	private static Comparator<Composition> byRecord = (c1, c2) -> c1.getRecordType().toString().compareToIgnoreCase(c2.getRecordType().toString());
 	private static Comparator<Composition> byTitre = (c1, c2) -> c1.getTitre().compareToIgnoreCase(c2.getTitre());
 	private static Comparator<Composition> byArtist = (c1, c2) -> c1.getArtist().compareToIgnoreCase(c2.getArtist());
-	
-	private ImportXML() {}
+
+	private ImportXML() {
+	}
 
 	/**
 	 * Import le fichier donné et extrait les {@link Composition}.
+	 * 
 	 * @param uri {@link String} le chemin absolu du fichier
 	 * @return les compos extraites
 	 */
@@ -75,7 +77,7 @@ public final class ImportXML {
 	 * history) et renvoie la liste de Composition.
 	 * 
 	 * @param dirName le dossier où se situe les fichiers
-	 * @param resultLabel {@link JTextArea} la zone de texte pour afficher 
+	 * @param resultLabel {@link JTextArea} la zone de texte pour afficher
 	 * @return la liste des {@link Composition} des fichiers
 	 * @throws IOException
 	 */
@@ -84,17 +86,35 @@ public final class ImportXML {
 		File dir = new File(dirName);
 		List<File> files = new ArrayList<>();
 		CompositionUtils.listFilesForFolder(dir, files, Constant.XML_EXTENSION, false);
-		List<Composition> compoFusion = new ArrayList<>();
+		List<Composition> compoFusionSong = new ArrayList<>();
+		List<Composition> compoFusionAlbum = new ArrayList<>();
 		for (File fileXML : files) {
 			boolean isFinal = Constant.FINAL_FILE.equalsIgnoreCase(fileXML.getName());
 			if (!isFinal) {
-				compoFusion.addAll(ImportXML.importXML(fileXML.getAbsolutePath()));
+				List<Composition> importXML = ImportXML.importXML(fileXML.getAbsolutePath());
+				if (RecordType.ALBUM.equals(importXML.get(0).getRecordType())) {
+					compoFusionAlbum.addAll(importXML);
+				} else {
+					compoFusionSong.addAll(importXML);
+				}
 			}
 		}
-		// compoFusion = compoFusion.stream().sorted(byRecord.thenComparing(byTitre).thenComparing(byArtist)).collect(Collectors.toList());
+		// compoFusion =
+		// compoFusion.stream().sorted(byRecord.thenComparing(byTitre).thenComparing(byArtist)).collect(Collectors.toList());
 		List<Composition> compoFinal = new ArrayList<>();
-		int size = compoFusion.size();
-		Double i = 0D;
+		BigDecimal sizeBG = new BigDecimal(compoFusionAlbum.size() + compoFusionSong.size());
+		int modulo = Math.round(sizeBG.divide(BigDecimal.valueOf(5000D)).floatValue());
+		LOG.debug(modulo);
+		LOG.debug(sizeBG.intValue());
+		compoFinal = fusion(resultLabel, compoFusionAlbum, compoFinal, 0, modulo, sizeBG);
+		compoFinal = fusion(resultLabel, compoFusionSong, compoFinal, compoFusionAlbum.size(), modulo, sizeBG);
+		ExportXML.exportXML(compoFinal, Constant.FINAL_FILE);
+		LOG.debug("End fusionFiles");
+		return compoFinal;
+	}
+
+	private static List<Composition> fusion(final JTextArea resultLabel, List<Composition> compoFusion, List<Composition> compoFinal, int i, int modulo,
+			BigDecimal sizeBG) {
 		for (Composition compo : compoFusion) {
 			Composition compoExist = CompositionUtils.compoExist(compoFinal, compo);
 			if (compoExist == null) {
@@ -102,20 +122,18 @@ public final class ImportXML {
 			} else {
 				compoExist.getFiles().addAll(compo.getFiles());
 			}
-			if(i%800==0) {
-				String msg = "Fusion à " + DECIMAL_FORMAT.format(100*i/size) + "%";
-				LOG.info(msg);
-				updateResultLabel(Arrays.asList(msg), resultLabel);
+			if (i % modulo == 0) {
+				updateResultLabel(
+						Arrays.asList("Fusion à "
+								+ BigDecimal.valueOf(100D).setScale(2).multiply(new BigDecimal(i)).divide(sizeBG, RoundingMode.HALF_UP).doubleValue() + "%"),
+						resultLabel);
 			}
 			i++;
 		}
-		ExportXML.exportXML(compoFinal, Constant.FINAL_FILE);
-		LOG.debug("End fusionFiles");
 		return compoFinal;
 	}
-	
-	private static void updateResultLabel(List<String> result2, final JTextArea resultLabel ) {
-		LOG.debug("Start updateResultLabel");
+
+	private static void updateResultLabel(List<String> result2, final JTextArea resultLabel) {
 		StringBuilder s = new StringBuilder();
 		for (String string : result2) {
 			s.append(string).append(Constant.NEW_LINE);
@@ -124,7 +142,6 @@ public final class ImportXML {
 		resultLabel.setForeground(new Color(243, 16, 16));
 		Font labelFont = resultLabel.getFont();
 		resultLabel.setFont(new Font(labelFont.getName(), labelFont.getStyle(), 20));
-		LOG.debug("End updateResultLabel");
 	}
 
 	/**
