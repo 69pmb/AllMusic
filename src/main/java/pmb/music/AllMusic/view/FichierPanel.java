e388a65df89b6812e750d3a0f8c9b468a3094f7b
pierre-marie broca <pierre-marie.broca@axess.fr>
2018-05-30 15:13:18 +0200

    copy in clipboard when click left on row composition

--- src/main/java/pmb/music/AllMusic/view/FichierPanel.java
+++ src/main/java/pmb/music/AllMusic/view/FichierPanel.java
@@ -5,6 +5,9 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Point;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 import java.awt.event.ActionEvent;
 import java.awt.event.InputEvent;
 import java.awt.event.MouseAdapter;
@@ -352,9 +355,28 @@ public class FichierPanel extends AbstractPanel {
 			LOG.debug("End right mouse");
 		}
 	}
-	
+
+	@SuppressWarnings("unchecked")
 	private void mouseActionForCompoTable(MouseEvent e) {
-		
+		if (e.getClickCount() == 2 && (e.getModifiers() & InputEvent.BUTTON1_MASK) != 0) {
+			LOG.debug("Start left mouse");
+			LOG.debug("End left mouse");
+		} else if (SwingUtilities.isRightMouseButton(e)) {
+			LOG.debug("Start right mouse");
+			// Copie dans le clipboard l'artist et l'oeuvre
+			JTable target = (JTable) e.getSource();
+			int rowAtPoint = target
+					.rowAtPoint(SwingUtilities.convertPoint(target, new Point(e.getX(), e.getY()), target));
+			if (rowAtPoint > -1) {
+				target.setRowSelectionInterval(rowAtPoint, rowAtPoint);
+			}
+			Vector<String> v = (Vector<String>) ((CompoModel) target.getModel()).getDataVector()
+					.get(target.getRowSorter().convertRowIndexToModel(rowAtPoint));
+			StringSelection selection = new StringSelection(v.get(0) + " " + v.get(1));
+			Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+			clipboard.setContents(selection, selection);
+			LOG.debug("End right mouse");
+		}
 	}
 
 	private void searchAction() {
