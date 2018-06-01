998cec15c2a677927bb5128f489b54155e6308e1
pierre-marie broca <pierre-marie.broca@axess.fr>
2018-06-01 16:31:18 +0200

    clean

--- src/main/java/pmb/music/AllMusic/view/EvenOddRenderer.java
+++ src/main/java/pmb/music/AllMusic/view/EvenOddRenderer.java
@@ -13,38 +13,38 @@ import javax.swing.table.TableCellRenderer;
 
 /**
  * Classe decrivant le style des tableaux de l'application.
- * Les couleurs des lignes sur 2.
+ * Colorise une ligne sur deux.
+ * @see DefaultTableCellRenderer
  */
 public class EvenOddRenderer extends DefaultTableCellRenderer implements TableCellRenderer {
 
     private static final long serialVersionUID = 7366600520755781512L;
 
-    public static final DefaultTableCellRenderer DEFAULT_RENDERER = new DefaultTableCellRenderer();
-
     @Override
-    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-        Component renderer = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
-        Color foreground;
-        Color background;
-        Color blue = new Color(47, 129, 210);
-        Color darkBlue = new Color(33, 93, 153);
-        Color gray = new Color(238, 229, 222);
-        table.setBorder(noFocusBorder);
-        if (isSelected) {
-            setBorder(new MatteBorder(1, 0, 1, 0, Color.black));
-            foreground = Color.black;
-            background = darkBlue;
-        } else {
-            if (row % 2 == 0) {
-                foreground = blue;
-                background = gray;
-            } else {
-                foreground = gray;
-                background = blue;
-            }
-        }
-        renderer.setForeground(foreground);
-        renderer.setBackground(background);
-        return renderer;
-    }
+	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
+			int row, int column) {
+		Component renderer = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+		Color foreground;
+		Color background;
+		Color blue = new Color(47, 129, 210);
+		Color darkBlue = new Color(33, 93, 153);
+		Color gray = new Color(238, 229, 222);
+		table.setBorder(noFocusBorder);
+		if (isSelected) {
+			setBorder(new MatteBorder(1, 0, 1, 0, Color.black));
+			foreground = Color.black;
+			background = darkBlue;
+		} else {
+			if (row % 2 == 0) {
+				foreground = blue;
+				background = gray;
+			} else {
+				foreground = gray;
+				background = blue;
+			}
+		}
+		renderer.setForeground(foreground);
+		renderer.setBackground(background);
+		return renderer;
+	}
 }
