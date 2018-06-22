580cd77d7a5679c3859375ed57ba5034014299ca
pierre-marie broca <pierre-marie.broca@axess.fr>
2018-06-22 14:51:22 +0200

    AbstractCompoModel -> AbstractModel, FichierModel for each Table Fichier

---
+++ src/main/java/pmb/music/AllMusic/view/model/FichierPanelModel.java
@@ -0,0 +1,61 @@
+/**
+ * 
+ */
+package pmb.music.AllMusic.view.model;
+
+import java.util.List;
+import java.util.Vector;
+
+import javax.swing.table.DefaultTableModel;
+
+/**
+ * Décrit la façon dont les tableaux contenant des {@code Fichier} seront
+ * affichées pour l'onglet {@code FichierPanel}.
+ * 
+ * @see {@link DefaultTableModel}
+ * @author pmbroca
+ */
+public class FichierPanelModel extends AbstractModel {
+
+	private static final long serialVersionUID = 1L;
+
+	private static final int INDEX_PUBLISH_YEAR = 2;
+	private static final int INDEX_FILE_SIZE = 6;
+	private static final int INDEX_RANK = 7;
+
+	/**
+	 * Constructeur de {@link FichierPanelModel}.
+	 * 
+	 * @param data {@code Object[][]} les données
+	 * @param title {@code String[]} les entetes du tableau
+	 */
+	public FichierPanelModel(Object[][] data, String[] title) {
+		super(data, title);
+	}
+
+	/**
+	 * Constructeur de {@link FichierPanelModel}.
+	 * 
+	 * @param data {@link Vector} les données
+	 * @param title {@link Vector} les entetes du tableau
+	 */
+	@SuppressWarnings("rawtypes")
+	public FichierPanelModel(Vector data, Vector title) {
+		super(data, title);
+	}
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@Override
+	public Class getColumnClass(int col) {
+		if (col == INDEX_PUBLISH_YEAR || col == INDEX_FILE_SIZE || col == INDEX_RANK) {
+			return Integer.class;
+		} else {
+			return String.class;
+		}
+	}
+
+	@Override
+	public List<Object> getSelected() {
+		return null;
+	}
+}
