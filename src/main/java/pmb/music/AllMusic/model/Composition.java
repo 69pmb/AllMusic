5c37e444a3e7562957c5dc247a481c3c490b79f8
pierre-marie broca <pierre-marie.broca@axess.fr>
2018-07-12 13:55:42 +0200

    delete equals and hashcode

--- src/main/java/pmb/music/AllMusic/model/Composition.java
+++ src/main/java/pmb/music/AllMusic/model/Composition.java
@@ -6,11 +6,10 @@ package pmb.music.AllMusic.model;
 import java.io.Serializable;
 import java.util.List;
 
-import org.apache.commons.collections.CollectionUtils;
-
 /**
  * Une composition est ensemble composé d'un artiste et d'une oeuvre musicale
  * qui est listé dans au moins un {@link Fichier}.
+ * 
  * @author pmbroca
  */
 public class Composition implements Serializable {
@@ -23,6 +22,7 @@ public class Composition implements Serializable {
 
 	/**
 	 * Constructeur.
+	 * 
 	 * @param artist un artiste (ou un groupe de musique)
 	 * @param files les fichier contenant la composition
 	 * @param titre le titre
@@ -35,7 +35,7 @@ public class Composition implements Serializable {
 		this.titre = titre;
 		this.recordType = recordType;
 	}
-	
+
 	public Composition(Composition compo) {
 		super();
 		this.artist = compo.getArtist();
@@ -119,58 +119,7 @@ public class Composition implements Serializable {
 	 */
 	@Override
 	public String toString() {
-		return "Composition [artist=" + this.artist + ", titre=" + this.titre + ", recordType=" + this.recordType + ", files=" + this.files + "]";
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Object#hashCode()
-	 */
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((this.artist == null) ? 0 : this.artist.hashCode());
-		result = prime * result + ((this.files == null) ? 0 : this.files.hashCode());
-		result = prime * result + ((this.titre == null) ? 0 : this.titre.hashCode());
-		result = prime * result + ((this.recordType == null) ? 0 : this.recordType.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Composition other = (Composition) obj;
-		if (this.artist == null) {
-			if (other.artist != null)
-				return false;
-		} else if (!this.artist.equals(other.artist))
-			return false;
-		if (CollectionUtils.isEmpty(this.files)) {
-			if (CollectionUtils.isEmpty(other.files))
-				return false;
-		} else if (this.files.size() != other.files.size()) {
-			return false;
-		} else {
-			for (int i = 0; i < this.files.size(); i++) {
-				if (!this.files.get(i).equals(other.files.get(i))) {
-					return false;
-				}
-			}
-		}
-		if (this.titre == null) {
-			if (other.titre != null)
-				return false;
-		} else if (!this.titre.equals(other.titre))
-			return false;
-		if (this.recordType != other.recordType)
-			return false;
-		return true;
+		return "Composition [artist=" + this.artist + ", titre=" + this.titre + ", recordType=" + this.recordType
+				+ ", files=" + this.files + "]";
 	}
 }
