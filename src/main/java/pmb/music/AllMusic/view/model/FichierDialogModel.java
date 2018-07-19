aa6932f956a12e2b23f63bc7ae3fc217cd3f1177
pierre-marie broca <pierre-marie.broca@axess.fr>
2018-07-19 10:14:56 +0200

    Format

--- src/main/java/pmb/music/AllMusic/view/model/FichierDialogModel.java
+++ src/main/java/pmb/music/AllMusic/view/model/FichierDialogModel.java
@@ -9,48 +9,50 @@ import java.util.Vector;
 import javax.swing.table.DefaultTableModel;
 
 /**
- * Décrit la façon dont les tableaux contenant des {@code Fichier} seront affichées
- * pour {@code DialogFileTable}.
+ * Décrit la façon dont les tableaux contenant des {@code Fichier} seront
+ * affichées pour {@code DialogFileTable}.
  * 
  * @see {@link DefaultTableModel}
  * @author pmbroca
  */
 public class FichierDialogModel extends AbstractModel {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
 	private static final int INDEX_PUBLISH_YEAR = 5;
 	private static final int INDEX_FILE_SIZE = 8;
 	private static final int INDEX_RANK = 9;
 
-    /**
-     * Constructeur de {@link FichierDialogModel}.
-     * @param data {@code Object[][]} les données
-     * @param title {@code String[]} les entetes du tableau
-     */
-    public FichierDialogModel(Object[][] data, String[] title) {
-        super(data, title);
-    }
-    
-    /**
-     * Constructeur de {@link FichierDialogModel}.
-     * @param data {@link Vector} les données
-     * @param title {@link Vector} les entetes du tableau
-     */
-    @SuppressWarnings("rawtypes")
-    public FichierDialogModel(Vector data, Vector title) {
-        super(data, title);
-    }
+	/**
+	 * Constructeur de {@link FichierDialogModel}.
+	 * 
+	 * @param data {@code Object[][]} les données
+	 * @param title {@code String[]} les entetes du tableau
+	 */
+	public FichierDialogModel(Object[][] data, String[] title) {
+		super(data, title);
+	}
 
-    @SuppressWarnings({ "unchecked", "rawtypes" })
-    @Override
-    public Class getColumnClass(int col) {
-    	if (col == INDEX_PUBLISH_YEAR || col == INDEX_FILE_SIZE || col == INDEX_RANK) {
-            return Integer.class;
-        } else {
-            return String.class;
-        }
-    }
+	/**
+	 * Constructeur de {@link FichierDialogModel}.
+	 * 
+	 * @param data {@link Vector} les données
+	 * @param title {@link Vector} les entetes du tableau
+	 */
+	@SuppressWarnings("rawtypes")
+	public FichierDialogModel(Vector data, Vector title) {
+		super(data, title);
+	}
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@Override
+	public Class getColumnClass(int col) {
+		if (col == INDEX_PUBLISH_YEAR || col == INDEX_FILE_SIZE || col == INDEX_RANK) {
+			return Integer.class;
+		} else {
+			return String.class;
+		}
+	}
 
 	@Override
 	public List<Object> getSelected() {
