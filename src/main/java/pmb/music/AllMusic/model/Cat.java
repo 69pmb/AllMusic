1c6ca2de3372fe3309a8816855015903bdce9177
pierre-marie broca <pierre-marie.broca@axess.fr>
2018-09-12 11:58:02 +0200

    add Cat LONG_PERIOD for ranking of more than 10 years.

--- src/main/java/pmb/music/AllMusic/model/Cat.java
+++ src/main/java/pmb/music/AllMusic/model/Cat.java
@@ -5,13 +5,15 @@ package pmb.music.AllMusic.model;
 
 /**
  * Enumération des différentes catégories de liste d'oeuvres musicales: bilan
- * annuel, décennale, par thème, par genre, "all time" ou divers.
+ * annuel, décennale, par thème, par genre, sur une longue période de temps,
+ * "all time" ou divers.
  * 
  * @author pmbroca
  */
 public enum Cat {
 	YEAR("YEAR"), 
 	DECADE("DECADE"), 
+	LONG_PERIOD("LONG PERIOD"), 
 	THEME("THEME"), 
 	GENRE("GENRE"), 
 	ALL_TIME("ALL TIME"), 
@@ -25,5 +27,14 @@ public enum Cat {
 
 	public String getCat() {
 		return value;
+	}
+
+	public static Cat getByValue(String value) {
+		for (Cat cat : values()) {
+			if (cat.value == value) {
+				return cat;
+			}
+		}
+		return null;
 	}
 }
