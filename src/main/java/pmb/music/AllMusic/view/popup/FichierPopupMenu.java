f60e25c1f952f2d4eb55f982893879f6f229004e
pierre-marie broca <pierre-marie.broca@axess.fr>
2018-10-12 16:18:21 +0200

    clean

--- src/main/java/pmb/music/AllMusic/view/popup/FichierPopupMenu.java
+++ src/main/java/pmb/music/AllMusic/view/popup/FichierPopupMenu.java
@@ -30,9 +30,6 @@ public class FichierPopupMenu extends JPopupMenu {
 	private static final Logger LOG = Logger.getLogger(FichierPopupMenu.class);
 
 	private Vector<String> selectedRow;
-	private int fileNameIndex;
-	private int authorIndex;
-	private JTable table;
 	private Point point;
 
 	/**
@@ -45,9 +42,6 @@ public class FichierPopupMenu extends JPopupMenu {
 	public FichierPopupMenu(JTable table, int fileNameIndex, int authorIndex) {
 		super();
 		LOG.debug("Start FichierPopupMenu");
-		this.table = table;
-		this.fileNameIndex = fileNameIndex;
-		this.authorIndex = authorIndex;
 
 		// Open XML file
 		JMenuItem openXml = new JMenuItem("Ouvrir le fichier XML");
@@ -106,30 +100,6 @@ public class FichierPopupMenu extends JPopupMenu {
 		this.add(modifFile);
 
 		LOG.debug("End FichierPopupMenu");
-	}
-
-	public int getFileNameIndex() {
-		return fileNameIndex;
-	}
-
-	public void setFileNameIndex(int fileNameIndex) {
-		this.fileNameIndex = fileNameIndex;
-	}
-
-	public int getAuthorIndex() {
-		return authorIndex;
-	}
-
-	public void setAuthorIndex(int authorIndex) {
-		this.authorIndex = authorIndex;
-	}
-
-	public JTable getTable() {
-		return table;
-	}
-
-	public void setTable(JTable table) {
-		this.table = table;
 	}
 
 	public Vector<String> getSelectedRow() {
