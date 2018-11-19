6b361669bd6441874ab309976287b9cb2437feb4
pierre-marie broca <pierre-marie.broca@axess.fr>
2018-11-19 10:03:14 +0100

    add code in bat file

--- AllMusic.bat
+++ AllMusic.bat
@@ -1,13 +1,15 @@
 @echo off
 cd C:\Users\Pierre-Marie\git\AllMusic
 :debut
-set /p answer="1. CMD    2. Update    3.Start  "
+set /p answer="1. CMD    2. Code    3. Update    4.Start  "
 set "result=nothing"
 IF /i "%answer%"=="1" (
-	call "AllMusic - CMD.bat"
+	cmd.exe /K "cd ."
 ) else IF /i "%answer%"=="2" (
-	call "AllMusic - Update.bat"
+	call code .
 ) else IF /i "%answer%"=="3" (
+	call "AllMusic - Update.bat"
+) else IF /i "%answer%"=="4" (
 	call "AllMusic - Start.bat"
 ) else (
  goto debut
