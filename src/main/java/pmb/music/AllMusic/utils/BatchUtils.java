fb453062829537a2db2c67eb5258ccc68bfef227
pierre-marie broca <pierre-marie.broca@axess.fr>
2018-12-20 17:35:29 +0100

    Removes archiving files (history)

--- src/main/java/pmb/music/AllMusic/utils/BatchUtils.java
+++ src/main/java/pmb/music/AllMusic/utils/BatchUtils.java
@@ -11,12 +11,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 import java.text.DecimalFormat;
-import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.IntSummaryStatistics;
 import java.util.LinkedList;
@@ -549,67 +546,6 @@ public class BatchUtils {
 		}
 
 		LOG.debug("End topYear");
-		return writeInFile(text, Constant.BATCH_FILE);
-	}
-
-	/**
-	 * Supprime tous les fichiers historisés sauf le plus récent.
-	 * 
-	 * @return
-	 */
-	public static String cleanHistory() {
-		LOG.debug("Start cleanHistory");
-		StringBuilder text = new StringBuilder();
-		addLine(text, "Clean History: ", true);
-
-		// Création d'une map avec:
-		// key nom du fichier sans date
-		// value liste des dates du fichier
-		List<File> files = new ArrayList<>();
-		FichierUtils.listFilesForFolder(new File(Constant.getHistoryPath()), files, Constant.XML_EXTENSION, false);
-		int size = files.size();
-		Map<String, List<Date>> list = new HashMap<String, List<Date>>();
-		for (File file : files) {
-			if (!StringUtils.endsWithIgnoreCase(file.getName(), Constant.XML_EXTENSION)) {
-				continue;
-			}
-			String nomFichier = StringUtils.substringBefore(file.getName(), Constant.SEPARATOR_DATE_HISTORY);
-			String date = StringUtils.substringBetween(file.getName(), Constant.SEPARATOR_DATE_HISTORY,
-					Constant.XML_EXTENSION);
-			if (list.get(nomFichier) == null) {
-				list.put(nomFichier, new ArrayList<Date>());
-			}
-			try {
-				list.get(nomFichier).add(new Constant().getSdfHistory().parse(date));
-			} catch (ParseException e) {
-				addLine(text, "Erreur lors du parsing d'une date" + e.getMessage(), true);
-				LOG.error("Erreur lors du parsing d'une date", e);
-			}
-		}
-
-		Set<String> keySet = list.keySet();
-		for (String key : keySet) {
-			// Tri des dates, la plus récente en 1er
-			Collections.sort(list.get(key), Collections.reverseOrder());
-			String path = Constant.getHistoryPath() + key + Constant.SEPARATOR_DATE_HISTORY;
-			// Suppression des fichiers sauf du 1er
-			for (int i = 1; i < list.get(key).size(); i++) {
-				String toDelete = path + new Constant().getSdfHistory().format(list.get(key).get(i))
-						+ Constant.XML_EXTENSION;
-				if (!new File(toDelete).delete()) {
-					addLine(text, toDelete + " n'a pas pu etre supprimé", true);
-					LOG.error(toDelete + " n'a pas pu etre supprimé");
-				}
-			}
-		}
-		files = new ArrayList<>();
-		FichierUtils.listFilesForFolder(new File(Constant.getHistoryPath()), files, Constant.XML_EXTENSION, false);
-		int result = size - files.size();
-		addLine(text, "Nombres de fichiers avant: " + size, true);
-		addLine(text, "Nombres de fichiers après: " + files.size(), true);
-		addLine(text, "Nombres de fichiers supprimés: " + result, true);
-
-		LOG.debug("End cleanHistory");
 		return writeInFile(text, Constant.BATCH_FILE);
 	}
 
