586c19896e2846a168b9c84bdf7e60e898e763bb
Pierre-Marie Broca <pierre-marie.broca@worldline.com>
2019-08-22 15:42:46 +0000

    fix(uuid): refresh search & fichier after edit

--- src/main/java/pmb/music/AllMusic/view/panel/FichierPanel.java
+++ src/main/java/pmb/music/AllMusic/view/panel/FichierPanel.java
@@ -495,7 +495,7 @@ public class FichierPanel extends JPanel implements ModificationComposition {
 	@Override
 	public void modifyCompositionAction(Vector<String> selected) throws MyException {
 		LOG.debug("Start modifyCompositionAction");
-		PanelUtils.editCompositionAction(selected, compositionList, INDEX_COMPO_ARTIST, INDEX_COMPO_TITLE,
+		compositionList = PanelUtils.editCompositionAction(selected, compositionList, INDEX_COMPO_ARTIST, INDEX_COMPO_TITLE,
 				INDEX_COMPO_TYPE, INDEX_COMPO_DELETED, INDEX_COMPO_UUID);
 		updateCompoTable(compositionList, selectedFichierName, false);
 		LOG.debug("End modifyCompositionAction");
