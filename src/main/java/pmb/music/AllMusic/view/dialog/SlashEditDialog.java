7e88dc4114b3eb7a70762cb3faa45b2d08937e76
Pierre-Marie Broca <pierre-marie.broca@worldline.com>
2019-08-30 07:54:46 +0000

    feat(slash): display if splitted title can be merged

--- src/main/java/pmb/music/AllMusic/view/dialog/SlashEditDialog.java
+++ src/main/java/pmb/music/AllMusic/view/dialog/SlashEditDialog.java
@@ -3,14 +3,18 @@ package pmb.music.AllMusic.view.dialog;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.List;
 import java.util.StringJoiner;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
@@ -19,8 +23,10 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import pmb.music.AllMusic.XML.ImportXML;
 import pmb.music.AllMusic.model.Composition;
 import pmb.music.AllMusic.model.Fichier;
+import pmb.music.AllMusic.utils.CompositionUtils;
 import pmb.music.AllMusic.utils.Constant;
 import pmb.music.AllMusic.view.ComponentBuilder;
 import pmb.music.AllMusic.view.PanelUtils;
@@ -38,11 +44,28 @@ public class SlashEditDialog {
 	// Data
 	private Boolean sendData;
 	private int size;
+	private List<Composition> finalFile;
+	private Composition found;
 
 	// Components
 	private JTextPane compoFound;
 	private JTextField title1;
 	private JTextField title2;
+	private JLabel label1;
+	private JLabel label2;
+
+	public final FocusListener searchForMerge = new FocusListener() {
+		@Override
+		public void focusLost(FocusEvent e) {
+			setLabelsValues();
+		}
+
+		@Override
+		public void focusGained(FocusEvent e) {
+			JTextField source = (JTextField) e.getSource();
+			source.selectAll();
+		}
+	};
 
 	/**
 	 * Constructeur de {@link SlashEditDialog}.
@@ -57,6 +80,7 @@ public class SlashEditDialog {
 		this.size = size;
 		this.dialog.setResizable(true);
 		this.initComponent();
+		finalFile = ImportXML.importXML(Constant.getFinalFilePath());
 		LOG.debug("End SlashEditDialog");
 	}
 
@@ -64,20 +88,22 @@ public class SlashEditDialog {
 		LOG.debug("Start initComponent");
 		JPanel panel = PanelUtils.createBoxLayoutPanel(BoxLayout.Y_AXIS);
 		PanelUtils.setSizeByScreenSize(panel, 78, 58);
-		PanelUtils.setBorder(panel, Color.blue);
 
 		// Compo
 		JPanel compoPanel = PanelUtils.createBoxLayoutPanel(BoxLayout.Y_AXIS);
 		PanelUtils.setSizeByScreenSize(compoPanel, 60, 50);
-		PanelUtils.setBorder(compoPanel, Color.RED);
 		compoFound = ComponentBuilder.initJTextPaneComponent(new Color(21, 77, 153), 20);
 		compoPanel.add(compoFound);
 
 		// Text Fields
 		title1 = (JTextField) new ComponentBuilder<JTextField>(JTextField.class).withComponentWidth(500).withPanelWidth(500)
 				.withLabel("Titre 1").withParent(compoPanel).build();
+		title1.addFocusListener(searchForMerge);
+		label1 = (JLabel) new ComponentBuilder<JLabel>(JLabel.class).withLabelWidth(600).withPanelWidth(600).withParent(compoPanel).build();
 		title2 = (JTextField) new ComponentBuilder<JTextField>(JTextField.class).withComponentWidth(500).withPanelWidth(500)
 				.withLabel("Titre 2").withParent(compoPanel).build();
+		title2.addFocusListener(searchForMerge);
+		label2 = (JLabel) new ComponentBuilder<JLabel>(JLabel.class).withLabelWidth(600).withPanelWidth(600).withParent(compoPanel).build();
 		panel.add(compoPanel);
 
 		JPanel btnPanel = new JPanel();
@@ -88,6 +114,7 @@ public class SlashEditDialog {
 			if (StringUtils.isNotBlank(title1.getText()) && StringUtils.isNotBlank(title2.getText())) {
 				dialog.setVisible(false);
 				sendData = true;
+				finalFile = ImportXML.importXML(Constant.getFinalFilePath());
 			} else if (!StringUtils.endsWith(compoFound.getText(), "!")) {
 				compoFound.setText(compoFound.getText() + Constant.NEW_LINE + "Input field empty !");
 			}
@@ -116,6 +143,7 @@ public class SlashEditDialog {
 	 */
 	public void updateDialog(Composition found, int index) {
 		sendData = null;
+		this.found = found;
 		this.dialog.setTitle(
 				index + "/" + size + " - " + BigDecimal.valueOf(100D).setScale(2).multiply(new BigDecimal(index))
 						.divide(new BigDecimal(size), RoundingMode.HALF_UP).doubleValue() + "%");
@@ -134,6 +162,23 @@ public class SlashEditDialog {
 		String[] split = StringUtils.split(found.getTitre(), "/");
 		title1.setText(split[0]);
 		title2.setText(split[1]);
+		setLabelsValues();
+	}
+
+	private void setLabelsValues() {
+		label1.setText(findIfMergeable(found, title1.getText()));
+		label2.setText(findIfMergeable(found, title2.getText()));
+	}
+
+	private String findIfMergeable(Composition c, String title) {
+		Composition searched = new Composition(c);
+		searched.setTitre(title);
+		Composition compoExist = CompositionUtils.compoExist(finalFile, searched);
+		String result = "Pas de fusion trouvée";
+		if (compoExist != null) {
+			result = "Fusion possible avec " + compoExist.getArtist() + " - " + compoExist.getTitre() + " (" + compoExist.getFiles().size() + " fichier(s))";
+		}
+		return result;
 	}
 
 	public Boolean getSendData() {
