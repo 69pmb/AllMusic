8c8e724a9389e62f8b945ed03dfe989754a601b4
Pierre-Marie Broca <pierre-marie.broca@worldline.com>
2019-09-23 17:47:48 +0200

    feat(index): has method on columnIndex

--- src/main/java/pmb/music/AllMusic/view/ColumnIndex.java
+++ src/main/java/pmb/music/AllMusic/view/ColumnIndex.java
@@ -3,34 +3,13 @@ package pmb.music.AllMusic.view;
 import java.util.HashMap;
 import java.util.Map;
 
+import pmb.music.AllMusic.utils.MinorException;
+
 public class ColumnIndex {
     private Map<Index, Integer> mapper;
 
     public enum Index {
-        LINE_NUMBER,
-        ARTIST,
-        TITLE,
-        TYPE,
-        FILE_SIZE,
-        SCORE,
-        DECILE,
-        SELECTED,
-        DELETED,
-        UUID,
-        AUTHOR,
-        FILE_NAME,
-        PUBLISH,
-        CAT,
-        RANGE,
-        CREATE_DATE,
-        PERCENT_DELETED,
-        SORTED,
-        RANK,
-        NB_TOTAL,
-        SCORE_TOTAL,
-        SCORE_SONG,
-        SCORE_ALBUM,
-        SCORE_DELETED
+        LINE_NUMBER, ARTIST, TITLE, TYPE, FILE_SIZE, SCORE, DECILE, SELECTED, DELETED, UUID, AUTHOR, FILE_NAME, PUBLISH, CAT, RANGE, CREATE_DATE, PERCENT_DELETED, SORTED, RANK, NB_TOTAL, SCORE_TOTAL, SCORE_SONG, SCORE_ALBUM, SCORE_DELETED
     }
 
     public ColumnIndex() {
@@ -42,7 +21,15 @@ public class ColumnIndex {
         return this;
     }
 
+    public boolean has(Index index) {
+        return mapper.containsKey(index);
+    }
+
     public Integer get(Index index) {
-        return mapper.get(index);
+        Integer result = mapper.get(index);
+        if (result == null) {
+            throw new MinorException("Index: " + index + " is not present.");
+        }
+        return result;
     }
 }
