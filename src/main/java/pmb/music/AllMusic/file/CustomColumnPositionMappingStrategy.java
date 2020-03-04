8dd6ea031aa43d3bc84af824867dd1f23e00f8d5
Pierre-Marie Broca <pierre-marie.broca@atos.net>
2020-03-04 10:53:02 +0100

    feat: openCsv to 5.1

--- src/main/java/pmb/music/AllMusic/file/CustomColumnPositionMappingStrategy.java
+++ src/main/java/pmb/music/AllMusic/file/CustomColumnPositionMappingStrategy.java
@@ -1,20 +1,18 @@
 package pmb.music.AllMusic.file;
 
-import java.io.IOException;
 import java.lang.reflect.Field;
+import java.util.Arrays;
 import java.util.Collection;
-import java.util.Comparator;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.ResourceBundle;
-import java.util.stream.IntStream;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+import org.apache.commons.collections4.ListValuedMap;
 import org.apache.commons.collections4.MultiValuedMap;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.reflect.FieldUtils;
+import org.apache.commons.text.WordUtils;
 
-import com.opencsv.CSVReader;
-import com.opencsv.ICSVParser;
 import com.opencsv.bean.AbstractBeanField;
 import com.opencsv.bean.AbstractCsvConverter;
 import com.opencsv.bean.AbstractMappingStrategy;
@@ -22,31 +20,25 @@ import com.opencsv.bean.BeanField;
 import com.opencsv.bean.BeanFieldJoinIntegerIndex;
 import com.opencsv.bean.BeanFieldSingleValue;
 import com.opencsv.bean.BeanFieldSplit;
-import com.opencsv.bean.ComplexFieldMapEntry;
+import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvBindAndJoinByPosition;
 import com.opencsv.bean.CsvBindAndSplitByPosition;
 import com.opencsv.bean.CsvBindByName;
 import com.opencsv.bean.CsvBindByPosition;
 import com.opencsv.bean.CsvConverter;
 import com.opencsv.bean.CsvCustomBindByPosition;
-import com.opencsv.bean.FieldMap;
 import com.opencsv.bean.FieldMapByPosition;
-import com.opencsv.bean.FieldMapByPositionEntry;
 import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
 
 /**
- * Class describing how to parse csv file when using {@code OpenCsv}.
+ * Class describing how to parse csv file when using {@code OpenCsv}. This is a
+ * custom class to mix Column position mapping and Header column name mapping.
  *
  * @see AbstractMappingStrategy
+ * @see ColumnPositionMappingStrategy
  * @param <T> class model of the csv
  */
-public class CustomColumnPositionMappingStrategy<T>
-extends AbstractMappingStrategy<String, Integer, ComplexFieldMapEntry<String, Integer, T>, T> {
-
-    /**
-     * Whether the user has programmatically set the map from column positions to field names.
-     */
-    private boolean columnsExplicitlySet;
+public class CustomColumnPositionMappingStrategy<T> extends ColumnPositionMappingStrategy<T> {
 
     /**
      * The map from column position to {@link BeanField}.
@@ -54,64 +46,29 @@ extends AbstractMappingStrategy<String, Integer, ComplexFieldMapEntry<String, In
     private FieldMapByPosition<T> fieldMap;
 
     /**
-     * Holds a {@link java.util.Comparator} to sort columns on writing.
-     */
-    private Comparator<Integer> writeOrder;
-
-    /**
-     * Used to store a mapping from presumed input column index to desired output column index, as determined by applying {@link #writeOrder}.
+     * Used to store a mapping from presumed input column index to desired output
+     * column index, as determined by applying {@link #writeOrder}.
      */
     private Integer[] columnIndexForWriting;
 
     public CustomColumnPositionMappingStrategy() {
-        // Nothing to do
+        super();
     }
 
-    @Override
-    public void captureHeader(CSVReader reader) throws IOException {
-        // Validation
-        if (type == null) {
-            throw new IllegalStateException(
-                    ResourceBundle.getBundle(ICSVParser.DEFAULT_BUNDLE_NAME, errorLocale).getString("type.unset"));
-        }
-
-        String[] firstLine = reader.peek();
-        fieldMap.setMaxIndex(firstLine.length - 1);
-        if (!columnsExplicitlySet) {
-            headerIndex.clear();
-            for (FieldMapByPositionEntry<T> entry : fieldMap) {
-                Field f = entry.getField().getField();
-                if (f.getAnnotation(CsvCustomBindByPosition.class) != null
-                        || f.getAnnotation(CsvBindAndSplitByPosition.class) != null
-                        || f.getAnnotation(CsvBindAndJoinByPosition.class) != null
-                        || f.getAnnotation(CsvBindByPosition.class) != null) {
-                    headerIndex.put(entry.getPosition(), f.getName().toUpperCase().trim());
-                }
-            }
+    public CustomColumnPositionMappingStrategy(Class<? extends T> type, String[] columnMapping) {
+        super();
+        this.type = type;
+        if (columnMapping != null) {
+            setColumnMapping(columnMapping);
+        } else {
+            this.setColumnMapping(FieldUtils.getFieldsListWithAnnotation(type, CsvBindByName.class).stream()
+                    .map(Field::getName).collect(Collectors.toList()).toArray(String[]::new));
         }
     }
 
     /**
-     * @return {@inheritDoc} For this mapping strategy, it's simply {@code index} wrapped as an {@link java.lang.Integer}.
-     */
-    // The rest of the Javadoc is inherited
-    @Override
-    protected Object chooseMultivaluedFieldIndexFromHeaderIndex(int index) {
-        return Integer.valueOf(index);
-    }
-
-    @Override
-    public BeanField<T> findField(int col) {
-        // If we have a mapping for changing the order of the columns on
-        // writing, be sure to use it.
-        if (columnIndexForWriting != null) {
-            return col < columnIndexForWriting.length ? fieldMap.get(columnIndexForWriting[col]) : null;
-        }
-        return fieldMap.get(col);
-    }
-
-    /**
-     * This method returns an empty array. The column position mapping strategy assumes that there is no header, and thus it also does not write one,
+     * This method returns an empty array. The column position mapping strategy
+     * assumes that there is no header, and thus it also does not write one,
      * accordingly.
      *
      * @return An empty array
@@ -119,101 +76,66 @@ extends AbstractMappingStrategy<String, Integer, ComplexFieldMapEntry<String, In
     // The rest of the Javadoc is inherited
     @Override
     public String[] generateHeader(T bean) throws CsvRequiredFieldEmptyException {
-        final int numColumns = findMaxFieldIndex();
+        // Custom method
+        final int numColumns = headerIndex.findMaxIndex();
         if (numColumns == -1) {
             return super.generateHeader(bean);
         }
         columnIndexForWriting = new Integer[numColumns + 1];
-
-        // Once we support Java 8, this might be nicer with Arrays.parallelSetAll().
-        for (int i = 0 ; i < columnIndexForWriting.length ; i++) {
-            columnIndexForWriting[i] = i;
-        }
-
+        Arrays.setAll(columnIndexForWriting, i -> i);
         String[] header = new String[numColumns + 1];
-
-        BeanField<?> beanField;
-        for (int i = 0 ; i <= numColumns ; i++) {
-            beanField = findField(i);
-            String columnHeaderName = extractHeaderName(beanField);
-            header[i] = columnHeaderName;
-        }
+        List<Field> fields = FieldUtils.getFieldsListWithAnnotation(bean.getClass(), CsvBindByName.class);
+        Arrays.setAll(header, i -> extractHeaderName(fields.get(i)));
         return header;
     }
 
-    private static String extractHeaderName(final BeanField<?> beanField) {
-        if (beanField == null || beanField.getField() == null
-                || beanField.getField().getDeclaredAnnotationsByType(CsvBindByName.class).length == 0) {
+    private String extractHeaderName(final Field field) {
+        // Custom method
+        if (field == null || field.getDeclaredAnnotationsByType(CsvBindByName.class).length == 0) {
             return StringUtils.EMPTY;
         }
 
-        final CsvBindByName bindByNameAnnotation = beanField.getField()
-                .getDeclaredAnnotationsByType(CsvBindByName.class)[0];
-        return bindByNameAnnotation.column();
+        String column = field.getDeclaredAnnotationsByType(CsvBindByName.class)[0].column();
+        String name = field.getName();
+        return WordUtils.capitalize(StringUtils.isBlank(column) ? name : column);
     }
 
     /**
-     * Gets a column name.
-     *
-     * @param col Position of the column.
-     * @return Column name or null if col &gt; number of mappings.
+     * Creates a map of annotated fields in the bean to be processed.
+     * <p>
+     * This method is called by {@link #loadFieldMap()} when at least one relevant
+     * annotation is found on a member variable.
+     * </p>
      */
     @Override
-    public String getColumnName(int col) {
-        return headerIndex.getByPosition(col);
-    }
-
-    /**
-     * Retrieves the column mappings.
-     *
-     * @return String array with the column mappings.
-     */
-    public String[] getColumnMapping() {
-        return headerIndex.getHeaderIndex();
-    }
-
-    /**
-     * Setter for the column mapping. This mapping is for reading. Use of this method in conjunction with writing is undefined.
-     *
-     * @param columnMapping Column names to be mapped.
-     */
-    public void setColumnMapping(String... columnMapping) {
-        if (columnMapping != null) {
-            headerIndex.initializeHeaderIndex(columnMapping);
-        } else {
-            headerIndex.clear();
-        }
-        columnsExplicitlySet = true;
-    }
-
-    @SuppressWarnings("rawtypes")
-    @Override
-    protected void loadFieldMap() {
+    protected void loadAnnotatedFieldMap(ListValuedMap<Class<?>, Field> fields) {
         boolean required;
-        fieldMap = new FieldMapByPosition<>(errorLocale);
-        fieldMap.setColumnOrderOnWrite(writeOrder);
         int i = 0;
-        for (Field field : loadFields(getType())) {
-            String fieldLocale;
-            String capture;
-            String format;
+        for (Map.Entry<Class<?>, Field> classAndField : fields.entries()) {
+            Class<?> localType = classAndField.getKey();
+            Field localField = classAndField.getValue();
+            String fieldLocale, fieldWriteLocale, capture, format;
 
             // Custom converters always have precedence.
-            if (field.isAnnotationPresent(CsvCustomBindByPosition.class)) {
-                CsvCustomBindByPosition annotation = field.getAnnotation(CsvCustomBindByPosition.class);
-                Class<? extends AbstractBeanField> converter = annotation.converter();
-                BeanField<T> bean = instantiateCustomConverter(converter);
-                bean.setField(field);
+            if (localField.isAnnotationPresent(CsvCustomBindByPosition.class)) {
+                CsvCustomBindByPosition annotation = localField.getAnnotation(CsvCustomBindByPosition.class);
+                @SuppressWarnings("unchecked")
+                Class<? extends AbstractBeanField<T, Integer>> converter = (Class<? extends AbstractBeanField<T, Integer>>) annotation
+                        .converter();
+                BeanField<T, Integer> bean = instantiateCustomConverter(converter);
+                bean.setType(localType);
+                bean.setField(localField);
                 required = annotation.required();
                 bean.setRequired(required);
                 fieldMap.put(annotation.position(), bean);
             }
 
             // Then check for a collection
-            else if (field.isAnnotationPresent(CsvBindAndSplitByPosition.class)) {
-                CsvBindAndSplitByPosition annotation = field.getAnnotation(CsvBindAndSplitByPosition.class);
+            else if (localField.isAnnotationPresent(CsvBindAndSplitByPosition.class)) {
+                CsvBindAndSplitByPosition annotation = localField.getAnnotation(CsvBindAndSplitByPosition.class);
                 required = annotation.required();
                 fieldLocale = annotation.locale();
+                fieldWriteLocale = annotation.writeLocaleEqualsReadLocale() ? fieldLocale : annotation.writeLocale();
                 String splitOn = annotation.splitOn();
                 String writeDelimiter = annotation.writeDelimiter();
                 Class<? extends Collection> collectionType = annotation.collectionType();
@@ -222,115 +144,61 @@ extends AbstractMappingStrategy<String, Integer, ComplexFieldMapEntry<String, In
                 capture = annotation.capture();
                 format = annotation.format();
 
-                CsvConverter converter = determineConverter(field, elementType, fieldLocale, splitConverter);
-                fieldMap.put(annotation.position(), new BeanFieldSplit<T>(field, required, errorLocale, converter,
-                        splitOn, writeDelimiter, collectionType, capture, format));
+                CsvConverter converter = determineConverter(localField, elementType, fieldLocale, fieldWriteLocale,
+                        splitConverter);
+                fieldMap.put(annotation.position(), new BeanFieldSplit<>(localType, localField, required, errorLocale,
+                        converter, splitOn, writeDelimiter, collectionType, capture, format));
             }
 
             // Then check for a multi-column annotation
-            else if (field.isAnnotationPresent(CsvBindAndJoinByPosition.class)) {
-                CsvBindAndJoinByPosition annotation = field.getAnnotation(CsvBindAndJoinByPosition.class);
+            else if (localField.isAnnotationPresent(CsvBindAndJoinByPosition.class)) {
+                CsvBindAndJoinByPosition annotation = localField.getAnnotation(CsvBindAndJoinByPosition.class);
                 required = annotation.required();
                 fieldLocale = annotation.locale();
+                fieldWriteLocale = annotation.writeLocaleEqualsReadLocale() ? fieldLocale : annotation.writeLocale();
                 Class<?> elementType = annotation.elementType();
                 Class<? extends MultiValuedMap> mapType = annotation.mapType();
                 Class<? extends AbstractCsvConverter> joinConverter = annotation.converter();
                 capture = annotation.capture();
                 format = annotation.format();
 
-                CsvConverter converter = determineConverter(field, elementType, fieldLocale, joinConverter);
-                fieldMap.putComplex(annotation.position(), new BeanFieldJoinIntegerIndex<T>(field, required,
-                        errorLocale, converter, mapType, capture, format));
+                CsvConverter converter = determineConverter(localField, elementType, fieldLocale, fieldWriteLocale,
+                        joinConverter);
+                fieldMap.putComplex(annotation.position(), new BeanFieldJoinIntegerIndex<>(localType, localField,
+                        required, errorLocale, converter, mapType, capture, format));
             }
 
             // By name annotation.
-            else if (field.isAnnotationPresent(CsvBindByName.class)) {
-                CsvBindByName annotation = field.getAnnotation(CsvBindByName.class);
+            // Custom method
+            else if (localField.isAnnotationPresent(CsvBindByName.class)) {
+                CsvBindByName annotation = localField.getAnnotation(CsvBindByName.class);
                 required = annotation.required();
                 fieldLocale = annotation.locale();
+                fieldWriteLocale = annotation.writeLocaleEqualsReadLocale() ? fieldLocale : annotation.writeLocale();
                 capture = annotation.capture();
                 format = annotation.format();
-                CsvConverter converter = determineConverter(field, field.getType(), fieldLocale, null);
 
-                fieldMap.put(i, new BeanFieldSingleValue<T>(field, required, errorLocale, converter, capture, format));
+                CsvConverter converter = determineConverter(localField, localField.getType(), fieldLocale,
+                        fieldWriteLocale, null);
+                fieldMap.put(i, new BeanFieldSingleValue<>(localField.getType(), localField, required, errorLocale,
+                        converter, capture, format));
             }
 
             // Then it must be a bind by position.
             else {
-                CsvBindByPosition annotation = field.getAnnotation(CsvBindByPosition.class);
+                CsvBindByPosition annotation = localField.getAnnotation(CsvBindByPosition.class);
                 required = annotation.required();
                 fieldLocale = annotation.locale();
+                fieldWriteLocale = annotation.writeLocaleEqualsReadLocale() ? fieldLocale : annotation.writeLocale();
                 capture = annotation.capture();
                 format = annotation.format();
-                CsvConverter converter = determineConverter(field, field.getType(), fieldLocale, null);
+                CsvConverter converter = determineConverter(localField, localField.getType(), fieldLocale,
+                        fieldWriteLocale, null);
 
-                fieldMap.put(annotation.position(),
-                        new BeanFieldSingleValue<T>(field, required, errorLocale, converter, capture, format));
+                fieldMap.put(annotation.position(), new BeanFieldSingleValue<>(localType, localField, required,
+                        errorLocale, converter, capture, format));
             }
             i++;
-        }
-    }
-
-    @Override
-    public void verifyLineLength(int numberOfFields) throws CsvRequiredFieldEmptyException {
-        if (!headerIndex.isEmpty()) {
-            StringBuilder sb = new StringBuilder();
-            IntStream.rangeClosed(numberOfFields, headerIndex.findMaxIndex()).mapToObj(this::findField)
-            .filter(f -> f != null && f.isRequired()).forEach(f -> {
-                if (sb.length() > 0) {
-                    sb.append(ResourceBundle.getBundle(ICSVParser.DEFAULT_BUNDLE_NAME, errorLocale)
-                            .getString("multiple.required.field.empty"));
-                }
-                sb.append(' ');
-                sb.append(f.getField().getName());
-            });
-            if (sb.length() > 0) {
-                throw new CsvRequiredFieldEmptyException(type, sb.toString());
-            }
-        }
-    }
-
-    private List<Field> loadFields(Class<? extends T> cls) {
-        List<Field> fields = new LinkedList<>();
-        for (Field field : FieldUtils.getAllFields(cls)) {
-            if (field.isAnnotationPresent(CsvBindByPosition.class)
-                    || field.isAnnotationPresent(CsvCustomBindByPosition.class)
-                    || field.isAnnotationPresent(CsvBindAndJoinByPosition.class)
-                    || field.isAnnotationPresent(CsvBindAndSplitByPosition.class)
-                    || field.isAnnotationPresent(CsvBindByName.class)) {
-                fields.add(field);
-            }
-        }
-        setAnnotationDriven(!fields.isEmpty());
-        return fields;
-    }
-
-    /**
-     * Returns the column position for the given column number. Yes, they're the same thing. For this mapping strategy, it's a simple conversion from
-     * an integer to a string.
-     */
-    // The rest of the Javadoc is inherited
-    @Override
-    public String findHeader(int col) {
-        return Integer.toString(col);
-    }
-
-    @Override
-    protected FieldMap<String, Integer, ? extends ComplexFieldMapEntry<String, Integer, T>, T> getFieldMap() {
-        return fieldMap;
-    }
-
-    /**
-     * Sets the {@link java.util.Comparator} to be used to sort columns when writing beans to a CSV file. Behavior of this method when used on a
-     * mapping strategy intended for reading data from a CSV source is not defined.
-     *
-     * @param writeOrder The {@link java.util.Comparator} to use. May be {@code null}, in which case the natural ordering is used.
-     * @since 4.3
-     */
-    public void setColumnOrderOnWrite(Comparator<Integer> writeOrder) {
-        this.writeOrder = writeOrder;
-        if (fieldMap != null) {
-            fieldMap.setColumnOrderOnWrite(this.writeOrder);
         }
     }
 }
